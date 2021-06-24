ee6a34d3155a1d2a9e6a466e4deb766877478541
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-06-24 14:59:48 +0600

    add amin page desktop styles

--- src/admin/AdminPanel.tsx
+++ src/admin/AdminPanel.tsx
@@ -8,11 +8,11 @@ export interface AdminPanelProps {
 const AdminPanel = (props: AdminPanelProps) => {
 
   return (
-    <div>
-      <header>
+    <div className="font-sans leading-normal tracking-normal flex flex-col md:flex-row bg-gray-100">
+      <aside className="bg-gray-800 shadow-xl h-16 fixed bottom-0 md:relative md:h-screen z-10 w-full md:w-60 md:pt-4 md:px-4">
         {props.navMenu}
-      </header>
-      <main>
+      </aside>
+      <main className="main-content flex-1 pb-24 md:pb-5 md:pt-4 md:px-4">
         {props.content}
       </main>
     </div>
--- src/admin/Navigation.tsx
+++ src/admin/Navigation.tsx
@@ -1,16 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Link } from "react-router-dom"
+import { faTemperatureLow, faCogs } from '@fortawesome/free-solid-svg-icons'
+
+const navItems = [
+  <Link to='/'><span className='w-10 inline-block'><FontAwesomeIcon icon={faTemperatureLow}/></span>Check the Weather</Link>,
+  <Link to='/configs'><span className='w-10 inline-block'><FontAwesomeIcon icon={faCogs} /></span>App configs</Link>
+]
+
+const navList = navItems.map(item => (
+  <li className='font-sans text-white flex flex-col md:flex-row py-1'>{ item }</li>
+))
 
 const Navigation = () => {
   return (
     <nav>
       <ul>
-        <li>
-          <Link to='/'>Check the Weather</Link>
-        </li>
-        <li>
-          <Link to='/configs'>App configs</Link>
-        </li>
+        {navList}
       </ul>
     </nav>
   )
