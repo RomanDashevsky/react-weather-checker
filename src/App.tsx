e08e735d1fb4aa44e016aecdf4b639fb45205cbe
Roman Dashevsky <dashevsky.roman@gmail.com>
2021-06-24 11:07:36 +0600

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './App.module.scss';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      <header className={classes["App-header"]}>
+        <p>
+          learn react
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
